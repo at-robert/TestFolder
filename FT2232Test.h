04d7778b6e17cef11d91e06ebdd8870137c5cc24
at-robert <atttrobert@gmail.com>
2015-06-23 23:28:18 +0800

    [Create] - To create an example for committing code onto Github

---
+++ FT2232Test.h
@@ -0,0 +1,31 @@
+// FT2232Test.h : PROJECT_NAME 應用程式的主要標頭檔
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "對 PCH 包含此檔案前先包含 'stdafx.h'"
+#endif
+
+#include "resource.h"		// 主要符號
+
+
+// CFT2232TestApp:
+// 請參閱實作此類別的 FT2232Test.cpp
+//
+
+class CFT2232TestApp : public CWinApp
+{
+public:
+	CFT2232TestApp();
+
+// 覆寫
+	public:
+	virtual BOOL InitInstance();
+
+// 程式碼實作
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CFT2232TestApp theApp;
